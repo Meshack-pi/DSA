c52e2432989d247e6a508fb365bd1acf278942a5
Meshack Kiprono <kipronomeshack36@gmail.com>
2025-04-22 08:26:03 +0300

    Add examples of arrays and while loops in C++ files

---
+++ C++/arrays.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //In this code I will be exploring the concept of arrays in cpp
+    //Arrays are used to store multiple values in a single variable instead of declaring separate values in it
+    //first define the variable type,specify the name of the array, followed by the square brackets which indicate the number of elements to store
+    //example
+    string cars[4] = {"volvo", "mercedes", "bmw", "honda"};
+    //you can create an array with any data type but the values of the elements must match the declared data type eg if array is a string then all elements must be a string and so forth
+    //accessing the values of an array
+    cout<<cars[3]<<endl;
+    //this will output honda since it is in the 4th position
+    //changing the value of an array
+    //reassigning it to a certain value
+
+    cars[2]= "not bmw anymore";
+    cout<<cars[2]<<endl;
+    //this will change the value of the cars[2] which was bmw now the value is "not bmw anymore"
+    //looping through an array we use the for looop since we already know the number of times the loop is supposed to run
+    cout<<"Loooping through an array using the for loop\n";
+    for (int i = 0; i<4; i++){
+        cout<<cars[i]<<endl;
+    }
+    //lets loop now using the for each looop in cpp
+    //this style was introduced in 2011 cpp version 11
+    for (string i : cars){
+        cout<<i<<endl;
+    }
+
+    //NB in CPP it is not a must to define the size of an array you can define the array with a number of values and the compiler will know the number of elements it contains
+    //for example
+    int numbers[] = {2,23,39,39,29,18, 5,7}; //8 elements
+    //it is also possible to declare an array with certain number of elements and give it values later on example below
+    int myStrangeNumbers[3];
+    myStrangeNumbers[0] = 3;
+    myStrangeNumbers[1] = 8;
+    myStrangeNumbers[2] = 4;
+    //This will not work for the variable that has not been given a distinct number of values eg the array int numbers[]; can't be used to do the same unless you add the number of elements
+
+    return 0;
+}
--- C++/whileloops.cpp
+++ C++/whileloops.cpp
@@ -49,6 +49,51 @@ int main(){
 
    //Nested loops is when we have several looops inside each other
    //A loop inside another loop
-   
-    return 0;
+   for (int i = 1; i <= 2; i++) {
+    for (int j = 1; j <= 3; j++) {
+      cout << i * j << "\n";
+    }
+  }
+  //WHAT IS THE OUTPUT OF THE ABOVE NESTED LOOP
+  //REMEMBER THE INNER LOOP IS SEEN AS A Block of code so it must be executed fully each time the outer loop is true;
+  //So the answer to the above is as follows:
+  /*
+  for the outer loop first value of i is 1 then inner loop is 1 
+  1 *1 is 1, then inner loop runs again with j as 2, so 1 * 2 is 2
+  then j is 3 so 1 *3 is 3
+  then the code block inside is terminated since 4 is not less than on equal to three so we start again with the outer loop
+  so for i = 2 so for j is 1 1*2 is 2
+  then loops again when j is 2 so 2 * 2 is 4
+  then for j is 3 so 3*2 is 6
+  so the final outputs is 1 2 3 2 4 6
+  //Most important thing to remember is that the inner loop is executed untill it is false for the outer loop to start once again
+  */
+ //C++ For each loop which is a range based loop
+ //must have an array of values 
+cout<<"C++ for each loop code\n";
+ int myNumbers[5] = {4,5,8,2,9};
+ for (int zed : myNumbers){
+    cout<<zed<<endl;
+ }
+ cout<<"C++ Break and continue key words";
+for (int i = 0; i<20;i++){
+    if (i==3){
+        break;
+    }
+    cout<<i<<endl;
+}
+//So the break keyword will make the code stop when the condition inside the block is true
+//in this case when the value of i is equal to 3 then the code block will terminate
+cout<<"continue statement in cpp\n";
+//the continue keywork will break one iteration in the loop if a specified condition is achieved
+//then continues with the next iteration in the loop
+for(int i = 0; i <=20; i++){
+    if(i==8){
+        continue;
+    }
+    cout<<i<<endl;
+}
+//The above statement has skipped 8 and gone to the next 
+
+return 0;
 }
